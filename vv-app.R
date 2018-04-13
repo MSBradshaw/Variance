ed33f67671c45829db6d760e116884d8de70bbdf
Michael <michaelscottbradshaw@gmail.com>
2018-04-13 17:55:37 -0600

    edited pileupfiles so they line up in the app

--- vv-app.R
+++ vv-app.R
@@ -28,6 +28,7 @@ server <- function(input, output) {
       return(NULL)
     }
     names <- c('8664_2','8664_3','8664_4','8664_5','8664_6')
+    names <- c('8663_A','8663_O','8663_T','8664_2','8664_3','8664_4','8664_5','8664_6','8665_N','8668_A','8668_B','8668_G','8678','8795_TA','8796','8796_TA','8797','8800','8801','8802_TC','8803_GA','8805','8806','8810','8935_B','8935_P')
    # names <- c('MCM7','RPB1','RPB2')
     paths <- c('final_dirs/8664_2/pileup.pileup',
                'final_dirs/8664_3/pileup.pileup',
@@ -35,7 +36,7 @@ server <- function(input, output) {
                'final_dirs/8664_5/pileup.pileup',
                'final_dirs/8664_6/pileup.pileup')
     #input$front,input$end
-    return( HTML( build_image( input$file1$datapath,input$image_type,'',0,0,names ) ) )
+    return( HTML( build_image( input$file1$datapath,input$image_type,'',input$front,input$end,names ) ) )
     })
 }
 shinyApp(ui = ui, server = server)
